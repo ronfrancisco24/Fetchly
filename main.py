2aa370b4391b0b1a07c83605290d6e4947d9209f
ronfrancisco24 <aaronmatt2004@gmail.com>
2025-09-26 22:31:13 +0800

    added initial files

---
+++ main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+import requests
+import time
+
+job_listings_url = "https://weworkremotely.com/sitemap.xml"
+technology_url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs#job-listings"
+movies_url = "https://api.themoviedb.org/3/trending/movie/week"
+
+requests.get()
+
+
+
